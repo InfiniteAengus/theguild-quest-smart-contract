36e6d4a27c662789208a9fd438652761cc307316
Cosmodude <vladislav.lenskii@gmail.com>
2023-11-27 16:47:02 +0900

    fixed escrow OZ + version

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,13 +1,11 @@
-pragma solidity 
+pragma solidity ^0.8.0;
 
-import "./openzeppelin/IERC20.sol";
-import "./openzeppelin/Address.sol";
-import "./openzeppelin//SafeERC20.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 contract Escrow {
 
-  using SafeERC20 for IERC20;
-  address public token;
+  address[] public tokens;
   address public governance; // who is it?
 
   modifier onlyGov() {
@@ -15,18 +13,18 @@ contract Escrow {
     _;
   }
 
-  constructor (address _token) public {
-    token = _token;
+  constructor (address[] _tokens) public {
+    tokens = _tokens;
     governance = msg.sender;
   }
 
   function approve(address _token, address to, uint256 amount) public onlyGov {
-    IERC20(_token).safeApprove(to, 0);
-    IERC20(_token).safeApprove(to, amount);
+    IERC20(_token).approve(to, 0);
+    IERC20(_token).approve(to, amount);
   }
 
   function transfer(address _token, address to, uint256 amount) public onlyGov {
-    IERC20(_token).safeTransfer(to, amount);
+    IERC20(_token).transfer(to, amount);
   }
 
   // This exists to mirror the interaction of how the perpetual staking pool would
