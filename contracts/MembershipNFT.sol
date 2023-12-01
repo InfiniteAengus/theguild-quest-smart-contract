d86da6bb72082e4fe7649601628f731e54f08827
Cosmodude <vladislav.lenskii@gmail.com>
2023-12-01 18:47:35 +0900

    cleaned up the imports, implemnted in OpenZeppelin

--- contracts/MembershipNFT.sol
+++ contracts/MembershipNFT.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "./Nft/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 import "./interfaces/INFTFactory.sol";
 import "./interfaces/IReferralHandler.sol";
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract MembershipNFT is ERC721URIStorage {
+    using SafeERC20 for IERC20;
     uint32 private _tokenIds;
     mapping(uint256 => address) public tokenMinter;
     address public admin;
