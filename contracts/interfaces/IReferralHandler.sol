4f610ca2cc77988d0c8c3f989ca89fd2860f3a2a
Cosmodude <vladislav.lenskii@gmail.com>
2024-01-12 18:28:24 +0900

    handler and profile i updates

--- contracts/interfaces/IReferralHandler.sol
+++ contracts/interfaces/IReferralHandler.sol
@@ -2,15 +2,18 @@
 pragma solidity ^0.8.0;
 
 interface IReferralHandler {
-    function initialize(address, address, address, uint256) external;
+    function initialize(
+        uint32 _referredBy,
+        address _nftAddress,
+        uint32 _nftId
+    ) external;
     function setTier(uint256 _tier) external;
-    function setDepositBox(address) external;
     function checkExistence(uint256, address) external view returns (address);
     function coupledNFT() external view returns (address);
-    function referredBy() external view returns (address);
+    function referredById() external view returns (uint32);
     function ownedBy() external view returns (address);
     function getTier() external view returns (uint8);
     function updateReferralTree(uint256 depth, uint8 NFTtier) external;
-    function addToReferralTree(uint256 depth, address referred, uint8 NFTtier) external;
+    function addToReferralTree(uint256 refDepth, uint32 referreId, uint8 _tier) external;
     function notifyFactory(uint256 reward, uint256 timestamp) external;
 }
