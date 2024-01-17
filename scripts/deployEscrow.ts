b433f1de5aa0bd847889f67e9528be5d18c19bfa
Cosmodude <vladislav.lenskii@gmail.com>
2024-01-17 19:19:53 +0900

    added erc6551 account + registry

--- scripts/deployEscrow.ts
+++ scripts/deployEscrow.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 
 async function main() {
 
-  const escrow = await ethers.deployContract("Escrow", [["0x6f9e2777D267FAe69b0C5A24a402D14DA1fBcaA1"]]);
+  const escrow = await ethers.deployContract("Escrow", [["0x6f9e2777D267FAe69b0C5A24a402D14DA1fBcaA1", "0x983a7B7D5d22c49887C21C98E6ADAaD6A860FF13"]]);
 
   await escrow.waitForDeployment();
 
