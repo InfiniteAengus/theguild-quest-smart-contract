2285caf245dba3267cde685154d7750d4a247762
Cosmodude <vladislav.lenskii@gmail.com>
2024-01-21 15:41:21 +0900

    cleaned uo tax manager interface

--- contracts/interfaces/ITaxManager.sol
+++ contracts/interfaces/ITaxManager.sol
@@ -11,18 +11,8 @@ interface ITaxManager {
     function getTierPool() external view returns (address);
     function getMarketingPool() external view returns (address);
     function getRevenuePool() external view returns (address);
-
-    function getSelfTaxRate() external view returns (uint256);
-    function getRightUpTaxRate() external view returns (uint256);
-    function getMaintenanceTaxRate() external view returns (uint256);
-    function getProtocolTaxRate() external view returns (uint256);
-    function getTotalTaxAtMint() external view returns (uint256);
-    function getPerpetualPoolTaxRate() external view returns (uint256);
-    function getTaxBaseDivisor() external view returns (uint256);
     
-    function getReferralRate(uint256, uint256) external view returns (uint256);
-    function getTierPoolRate() external view returns (uint256);
-    function getDevPoolRate() external view returns (uint256);
-    function getMarketingTaxRate() external view returns (uint256);
-    function getRewardPoolRate() external view returns (uint256);
+    function getProtocolTaxRate() external view returns (uint256);
+    function getTaxBaseDivisor() external view returns (uint256);
+    function getReferralRate(uint8 depth, uint8 tier) external view returns (uint256);
 }
