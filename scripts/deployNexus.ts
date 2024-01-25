fccb9eab3b90e9031d7081c0a696ba8cd097e4bc
Cosmodude <vladislav.lenskii@gmail.com>
2024-01-25 10:15:57 +0900

    deploy script huge update

--- scripts/deployNexus.ts
+++ scripts/deployNexus.ts
@@ -1,16 +1,10 @@
 import { ethers } from "hardhat";
 
+const registry = ethers.getAddress("0x000000006551c19487814612e58FE06813775758");
+
 async function main() {
 
-  const handler = await ethers.deployContract("ReferralHandler");
-
-  await handler.waitForDeployment();
-
-  console.log(
-    `Handler deployed to ${handler.target}`
-  );
-
-  const nexus = await ethers.deployContract("Nexus", [handler.target]);
+  const nexus = await ethers.deployContract("Nexus", ["0x6f9e2777D267FAe69b0C5A24a402D14DA1fBcaA1", registry]);
 
   await nexus.waitForDeployment();
 
@@ -26,15 +20,35 @@ async function main() {
     `Profile NFT deployed to ${nft.target}`
   );
 
+  const account = await ethers.deployContract("ReferralHandlerERC6551Account", [nexus.target]);
+
+  await account.waitForDeployment();
+
+  console.log(
+    `Handler Acoount deployed to ${account.target}`
+  );
+
   await nexus.setNFT(nft.target);
 
-  await nexus.setGuardian("0x2919e92Ef458D3D14D658159F24F7fDA200823D5");
+  console.log("Nft set to ", nft.target);
+
+  await nexus.setAccountImpl(account.target);
+
+  console.log("Account impl set to ", account.target);
+
+  await nexus.setGuardian("0x4CA23B523c2b0f730bC9BBc5152A286953159Fe5");
+
+  console.log("Guardian set to ", await nexus.getGuardian());
+
+  await nexus.setMaster("0x4CA23B523c2b0f730bC9BBc5152A286953159Fe5");
+
+  console.log("Master set to ", await nexus.master());
 
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+console.error(error);
+process.exitCode = 1;
 });
