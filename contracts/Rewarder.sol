3abd4d0d1c85c7db44853b3c8e04ebb84e85d2e2
Cosmodude <vladislav.lenskii@gmail.com>
2024-02-10 21:16:24 +0900

    docs + new function mocks

--- contracts/Rewarder.sol
+++ contracts/Rewarder.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: GNU AGPLv3
 pragma solidity ^0.8.0;
 
+import "./interfaces/IRewarder.sol";
 import "./interfaces/IReferralHandler.sol";
 import "./interfaces/INexus.sol";
 import "./interfaces/ITaxManager.sol";
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract Rewarder {
+contract Rewarder is IRewarder {
     using SafeERC20 for IERC20;
 
     uint256 public BASE = 1e18;
@@ -34,32 +35,56 @@ contract Rewarder {
         return ITaxManager(taxManager);
     }
 
-    function handleRewardNative(uint32 solverId) external payable { // anyone can call
+    /**
+     * @dev Can be called by anyone
+     * @param solverId Nft Id of the quest solver
+     */
+    function handleRewardNative(uint32 solverId) public payable {
         address escrow = msg.sender;
         require(escrow.balance == 0, "Escrow not empty");
 
         ITaxManager taxManager = getTaxManager();
         uint256 protocolTaxRate = taxManager.getProtocolTaxRate();
         uint256 taxRateDivisor = taxManager.getTaxBaseDivisor();
+
         uint256 rewardValue = msg.value;
         uint256 tax = (rewardValue * protocolTaxRate) / taxRateDivisor;
-        require(tax<= rewardValue);
-        
-        address solverHandler = nexus.getHandler(solverId);
-        address solver = IReferralHandler(solverHandler).ownedBy();
-
-        rewardReferrers(
-            solverHandler,
-            tax,
-            taxRateDivisor
-        );
-        
+        require(tax <= rewardValue);
         uint256 solverReward = rewardValue - tax;
-        (bool success, ) = payable(solver).call{value: solverReward }("");
-        require(success, "Solver reward pay error;");
+        address solverHandler = nexus.getHandler(solverId);
         emit RewardNativeClaimed(solverHandler, escrow, solverReward);
+
+        address solver = IReferralHandler(solverHandler).owner();
+
+        rewardReferrers(solverHandler, tax, taxRateDivisor);
+
+        (bool success, ) = payable(solver).call{value: solverReward}("");
+        require(success, "Solver reward pay error;");
     }
 
+    /**
+     * @dev Can be called by anyone
+     * @param solverId Nft Id of the quest solver
+     * @param token Address of the payment token contract
+     * @param amount Amount of the payment in token (with decimals)
+     */
+     function handleRewardToken(
+        address token,
+        uint32 solverId,
+        uint256 amount
+    ) external override {}
+
+    /**
+     * 
+     * @param seekerId Nft Id of the quest seeker
+     * @param solverId Nft Id of the quest solver
+     * @param solverShare % of the reward allocated to solver
+     */
+    function proccessResolutionNative(
+        uint32 seekerId,
+        uint32 solverId,
+        uint8 solverShare
+    ) external payable override {}
 
     // function handleSolverTax(
     //     address handler,
@@ -73,8 +98,8 @@ contract Rewarder {
     //     // uint256 taxedAmountReward = balance * selfTaxRate / divisor;
     //     // uint256 protocolTaxed = taxedAmountReward * protocolTaxRate / divisor;
     //     uint256 reward = 0;//taxedAmountReward - protocolTaxed;
-        
-    //     IReferralHandler(handler).notifyNexus(reward, block.timestamp); // change to notify 
+
+    //     IReferralHandler(handler).notifyNexus(reward, block.timestamp); // change to notify
     // }
 
     // function handlePayment(
@@ -91,7 +116,7 @@ contract Rewarder {
     // }
 
     function rewardReferrers(
-        address handler, 
+        address handler,
         uint256 taxValue,
         uint256 taxDivisor
     ) internal {
@@ -99,7 +124,7 @@ contract Rewarder {
         address[5] memory referrals; // Used to store above referrals, saving variable space
         uint256[5] memory rewards;
         // Block Scoping to reduce local Variables spillage
-        
+
         uint256 leftTax = taxValue;
 
         referrals[1] = IReferralHandler(handler).referredBy();
@@ -111,8 +136,8 @@ contract Rewarder {
                     1,
                     firstTier
                 );
-               
-                rewards[1] =  (taxValue * firstRewardRate) / taxDivisor;
+
+                rewards[1] = (taxValue * firstRewardRate) / taxDivisor;
                 leftTax -= rewards[1];
             }
             referrals[2] = IReferralHandler(referrals[1]).referredBy();
@@ -124,7 +149,7 @@ contract Rewarder {
                         2,
                         secondTier
                     );
-    
+
                     rewards[2] = (secondRewardRate * taxValue) / taxDivisor;
                     leftTax -= rewards[2];
                 }
@@ -132,22 +157,26 @@ contract Rewarder {
                 if (referrals[3] != address(0)) {
                     // Block Scoping to reduce local Variables spillage
                     {
-                        uint8 thirdTier = IReferralHandler(referrals[3]).getTier();
+                        uint8 thirdTier = IReferralHandler(referrals[3])
+                            .getTier();
                         uint256 thirdRewardRate = taxManager.getReferralRate(
                             3,
                             thirdTier
                         );
-                        rewards[3] =  (taxValue * thirdRewardRate) / taxDivisor;
+                        rewards[3] = (taxValue * thirdRewardRate) / taxDivisor;
                         leftTax -= rewards[3];
                     }
                     referrals[4] = IReferralHandler(referrals[3]).referredBy();
                     if (referrals[4] != address(0)) {
                         // Block Scoping to reduce local Variables spillage
                         {
-                            uint8 fourthTier = IReferralHandler(referrals[4]).getTier();
+                            uint8 fourthTier = IReferralHandler(referrals[4])
+                                .getTier();
                             uint256 fourthRewardRate = taxManager
                                 .getReferralRate(4, fourthTier);
-                            rewards[4] = (taxValue) * fourthRewardRate / taxDivisor;
+                            rewards[4] =
+                                ((taxValue) * fourthRewardRate) /
+                                taxDivisor;
                             leftTax -= rewards[4];
                         }
                     }
@@ -155,10 +184,10 @@ contract Rewarder {
             }
         }
         // Pay out the Refferal rewards
-        for(uint8 i =0; i<5 ; ++i){
+        for (uint8 i = 0; i < 5; ++i) {
             uint256 reward = rewards[i];
-            rewards[i]=0;
-            (bool success,) = payable(referrals[i]).call{value: reward}("");
+            rewards[i] = 0;
+            (bool success, ) = payable(referrals[i]).call{value: reward}("");
             require(success, "Referral rewards pay error;");
         }
         // // Reward Allocation
@@ -168,7 +197,7 @@ contract Rewarder {
         // Dev Allocation & // Revenue Allocation
         {
             address revenuePool = taxManager.getRevenuePool();
-            (bool success,) = payable(revenuePool).call{value: leftTax}("");
+            (bool success, ) = payable(revenuePool).call{value: leftTax}("");
             require(success, "Revenue pool pay error;");
         }
     }
@@ -177,12 +206,15 @@ contract Rewarder {
         address _token,
         address benefactor
     ) public onlySteward {
-        if(_token == address(0)){
-            (bool sent,) = payable(benefactor).call{value: address(this).balance}("");
+        if (_token == address(0)) {
+            (bool sent, ) = payable(benefactor).call{
+                value: address(this).balance
+            }("");
             require(sent, "Send error");
             return;
         }
         uint256 tokenBalance = IERC20(_token).balanceOf(address(this));
         IERC20(_token).transfer(benefactor, tokenBalance);
     }
+
 }
--- contracts/interfaces/IRewarder.sol
+++ contracts/interfaces/IRewarder.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.8.0;
 interface IRewarder {
     function handleRewardNative(uint32 solverId) external payable;
     function handleRewardToken(address token, uint32 solverId, uint256 amount) external;
+    function proccessResolutionNative(uint32 seekerId, uint32 solverId, uint8 solverShare) external payable;
 }
