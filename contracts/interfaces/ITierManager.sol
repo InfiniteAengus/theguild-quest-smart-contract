e4422b918a1edb6a4dd7b5277b193adbf970c305
syahirAmali <syahiramali@gmail.com>
2024-02-15 18:38:16 +0800

    Fixed data type

--- contracts/interfaces/ITierManager.sol
+++ contracts/interfaces/ITierManager.sol
@@ -3,5 +3,5 @@ pragma solidity ^0.8.0;
 
 interface ITierManager {
     function checkTierUpgrade(uint32[5] memory tierCounts) external returns (bool);
-    function getTokenURI(uint32 tokenId) external view returns (string memory tokenURI);
+    function getTokenURI(uint8 tier) external view returns (string memory tokenURI);
 }
