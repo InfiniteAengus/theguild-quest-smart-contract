0ac40db08dea4e7aba2b1c8b4079590beee8a364
syahirAmali <syahiramali@gmail.com>
2024-02-26 14:22:16 +0800

    Quest contract to support erc20

--- contracts/Quest.sol
+++ contracts/Quest.sol
@@ -1,10 +1,13 @@
 //SPDX-License-Identifier: GNU AGPLv3
 pragma solidity ^0.8.0;
 
-import { IEscrow } from "./interfaces/Quests/IEscrow.sol";
+import { IEscrowNative, IEscrowToken, IEscrow } from "./interfaces/Quests/IEscrow.sol";
 import { Clones } from "@openzeppelin/contracts/proxy/Clones.sol";
 import { IQuest } from "./interfaces/Quests/IQuest.sol";
 import { ITavern } from "./interfaces/Quests/ITavern.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 /**
  * @title Quest Implementation
  * @notice Controls the quest flow
@@ -13,14 +16,16 @@ import { ITavern } from "./interfaces/Quests/ITavern.sol";
  */
 
 contract Quest is IQuest {
+    using SafeERC20 for IERC20;
+
     // state variables
-    bool public initialized = false;
-    bool public started = false;
-    bool public submitted = false;
-    bool public extended = false;
-    bool public beingDisputed = false;
-    bool public finished = false;
-    bool public rewarded = false;
+    bool public initialized;
+    bool public started;
+    bool public submitted;
+    bool public extended;
+    bool public beingDisputed;
+    bool public finished;
+    bool public rewarded;
     bool public withToken;
 
     address public escrowImplemntation; // native or with token
@@ -30,10 +35,10 @@ contract Quest is IQuest {
     address public mediator;
     string public infoURI;
     uint256 public paymentAmount;
-    uint256 public rewardTime = 0;
+    uint256 public rewardTime;
     
     ITavern private Tavern;
-    IEscrow private escrow;
+    address private escrow;
     
     modifier onlySeeker() {
         require(Tavern.ownerOf(seekerId) == msg.sender, "only Seeker");
@@ -67,6 +72,7 @@ contract Quest is IQuest {
         infoURI = _infoURI;
         escrowImplemntation = _escrowImplementation;
         token = _token;
+        
         return true;
     }
 
@@ -81,8 +87,14 @@ contract Quest is IQuest {
            // todo tax logic 
         }
         started = true;
-        escrow = IEscrow(Clones.clone(escrowImplemntation));
-        escrow.initialize{value: msg.value}(token);
+        escrow = Clones.clone(escrowImplemntation);
+
+        if(token == address(0)){
+            IEscrowNative(escrow).initialize{value: msg.value}(token);
+        } else {
+            IERC20(token).transferFrom(msg.sender, escrow, paymentAmount);
+            IEscrowToken(escrow).initialize(token, paymentAmount);
+        }
     }
 
     // todo
@@ -90,7 +102,7 @@ contract Quest is IQuest {
         require(started, "quest not started");
         require(!beingDisputed, "Dispute started before");
         beingDisputed = true;
-        //mediator = Tavern.mediator();
+        mediator = Tavern.mediator();
     }
 
     function resolveDispute(
@@ -100,7 +112,7 @@ contract Quest is IQuest {
         require(!rewarded, "Rewarded before");
         require(solverShare <= 100, "Share can't be more than 100");
         rewarded = true;
-        escrow.proccessResolution(seekerId, solverId, solverShare, getRewarder());
+        IEscrow(escrow).proccessResolution(seekerId, solverId, solverShare, getRewarder());  
     }
 
     function finishQuest() external onlySolver {
@@ -113,8 +125,8 @@ contract Quest is IQuest {
 
     function extend() external onlySeeker {
         require(finished, "Quest not finished");
-        require(!rewarded, "Was rewarded before");
         require(!extended, "Was extended before");
+        require(!rewarded, "Was rewarded before");
         extended = true;
         rewardTime += Tavern.reviewPeriod();
     }
@@ -125,7 +137,7 @@ contract Quest is IQuest {
         require(!beingDisputed, "Is under dispute");
         require(rewardTime <= block.timestamp, "Not reward time yet");
         rewarded = true;
-        escrow.proccessPayment(solverId, getRewarder());
+        IEscrow(escrow).proccessPayment(solverId, getRewarder());
     }
 
     function getRewarder() public view returns (address) {
