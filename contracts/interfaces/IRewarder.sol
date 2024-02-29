527e0c963dd25fc23e339dd1cff8be007b5f7702
syahirAmali <syahiramali@gmail.com>
2024-02-29 09:49:18 +0800

    Rewarder updates

--- contracts/interfaces/IRewarder.sol
+++ contracts/interfaces/IRewarder.sol
@@ -6,4 +6,8 @@ interface IRewarder {
     function handleRewardToken(address token, uint32 solverId, uint256 amount) external;
     function proccessResolutionNative(uint32 seekerId, uint32 solverId, uint8 solverShare) external payable;
     function proccessResolutionToken(uint32 seekerId, uint32 solverId, uint8 solverShare, address token) external;
+
+    function calculateSeekerTax(uint256 paymentAmount) external returns (uint256 referralTax, uint256 platformTax);
+    function handleSeekerTaxNative(uint32 _solverId, uint256 _referralTaxAmount, uint256 _platformTaxAmount) external payable;
+    function handleSeekerTaxToken(uint32 _solverId, uint256 _referralTaxAmount, uint256 _platformTaxAmount, address token) external;
 }
