1dc056e4dc7a88c73eecd411e91f260b001b8d42
Syahir Amali <syahiramali@gmail.com>
2024-03-17 18:28:58 +0800

    Mock Token Updates

--- contracts/mocks/MockToken.sol
+++ contracts/mocks/MockToken.sol
@@ -4,8 +4,19 @@ pragma solidity 0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockToken is ERC20 {
-    
-    constructor() ERC20("Mock Token", "mTok") {}
+    uint8 public decimals_;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals
+    ) ERC20(_name, _symbol) {
+        decimals_ = _decimals;
+    }
+
+    function decimals() public view override returns (uint8) {
+        return decimals_;
+    }
 
     function mint(address _to, uint256 _amount) external {
         _mint(_to, _amount);
